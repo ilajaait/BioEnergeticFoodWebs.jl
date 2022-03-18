ab959ff37d9575d4588a7c2f858329963df6ed2f
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2022-03-18 15:36:02 +0100

    🎨 Use @testset in random.jl

--- test/random.jl
+++ test/random.jl
@@ -2,20 +2,22 @@ module TestRandom
     using Test
     using BioEnergeticFoodWebs
 
-    @test_throws AssertionError nichemodel(10, 150)
+    @testset "Niche model." begin
+        @test_throws AssertionError nichemodel(10, 150)
 
-    A = nichemodel(10, 20)
-    @test size(A, 1) == 10
-    @test size(A, 2) == 10
+        A = nichemodel(10, 20)
+        @test size(A, 1) == 10
+        @test size(A, 2) == 10
 
-    A = nichemodel(10, 0.12)
-    @test size(A, 1) == 10
-    @test size(A, 2) == 10
+        A = nichemodel(10, 0.12)
+        @test size(A, 1) == 10
+        @test size(A, 2) == 10
 
-    A = nichemodel(10, 0.12, toltype=:rel)
-    A = nichemodel(10, 0.12, toltype=:abs)
+        A = nichemodel(10, 0.12, toltype=:rel)
+        A = nichemodel(10, 0.12, toltype=:abs)
 
-    A = [0 1; 1 0]
-    @test BioEnergeticFoodWebs.connectance(A) == 0.5
+        A = [0 1; 1 0]
+        @test BioEnergeticFoodWebs.connectance(A) == 0.5
+    end
 
 end
